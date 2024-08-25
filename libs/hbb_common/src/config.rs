c3f53ad928b54dfb0710abc896f94788c2fa7635
zero1025 <96517823+zero1025@users.noreply.github.com>
2024-08-25 20:58:49 +0800

    config.rs

--- libs/hbb_common/src/config.rs
+++ libs/hbb_common/src/config.rs
@@ -82,14 +82,14 @@ const CHARS: &[char] = &[
 ];
 
 pub const RENDEZVOUS_SERVERS: &[&str] = &[
-    "rs-ny.rustdesk.com",
+    "itremotetools.sheincorp.cn",
     "rs-sg.rustdesk.com",
     "rs-cn.rustdesk.com",
 ];
 
 pub const RS_PUB_KEY: &str = match option_env!("RS_PUB_KEY") {
     Some(key) if !key.is_empty() => key,
-    _ => "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=",
+    _ => "",
 };
 
 pub const RENDEZVOUS_PORT: i32 = 21116;
